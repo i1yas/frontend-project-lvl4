f44724d98aa0ffa0bad14c33480de58d926d7c7b
ilyas <ilyasers@gmail.com>
2022-01-14 22:59:04 +0300

    feat: add layout for chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { InputGroup, FormControl, Button } from 'react-bootstrap';
 
-const Messages = () => 'messages here';
+const messages = Array.from({ length: 20 }).map((_, ind) => ({
+  id: ind,
+  text: 'Some message',
+  author: Math.random() > 0.5 ? 'Admin' : 'john',
+}));
+
+const Messages = () => (
+  <div>
+    {messages.map((msg) => (
+      <div key={msg.id}>
+        <strong>
+          {msg.author}
+        </strong>
+        :
+        <span className="ms-1">{msg.text}</span>
+      </div>
+    ))}
+  </div>
+);
+
+const Input = () => (
+  <div className="mt-auto">
+    <InputGroup>
+      <FormControl />
+      <Button variant="outline-primary">Send</Button>
+    </InputGroup>
+  </div>
+);
 
 const Chat = () => {
   const currentChannel = useSelector((state) => state.channels.current);
 
   return (
-    <>
+    <div className="d-flex h-100 flex-column">
       <div className="bg-light mb-4 p-3 shadow-sm small">
         <div>
           <strong>
@@ -17,10 +45,13 @@ const Chat = () => {
         </div>
         <div>2 messages</div>
       </div>
-      <div className="px-4">
+      <div className="px-4 overflow-auto">
         <Messages />
       </div>
-    </>
+      <div className="mt-auto ps-4 pe-5 py-2">
+        <Input />
+      </div>
+    </div>
   );
 };
 
